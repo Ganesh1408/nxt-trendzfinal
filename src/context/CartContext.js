e83996ba41bfdcc65ef0a855c4b9c2f90529c070
Ganesh1408 <121733704+Ganesh1408@users.noreply.github.com>
2023-05-24 22:28:59 +0530

    Update CartContext.js

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -4,6 +4,10 @@ const CartContext = React.createContext({
   cartList: [],
   addCartItem: () => {},
   deleteCartItem: () => {},
+   incrementCartItem: ()=>{},
+    removeAllCartItems: ()=>{},
+    decrementCartItem: ()=>{},
+
 })
 
 export default CartContext
